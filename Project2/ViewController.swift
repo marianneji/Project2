c6563d4b7a8c4b2648aa8f9942833d442106d230
marianneji <marianneji@gmail.com>
2019-07-12 11:45:39 +0200

    add IBAction button tapped UIAlert finish game

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
+    var questionAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +31,50 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
         askQuestion()
+        
     }
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        title = """
+        Guess: \(countries[correctAnswer].uppercased()) / Your score is: \(score)
+        """
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        endGame()
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        questionAsked += 1
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+           
+        } else {
+            title = "Wrong, that's the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+            
+        }
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        
+    }
+    func endGame() {
+        if questionAsked == 10 {
+            let ac2 = UIAlertController(title: "Game over", message: "Your final score is \(score)", preferredStyle: .alert)
+            ac2.addAction(UIAlertAction(title: "Ok", style: .default, handler: askQuestion))
+            present(ac2, animated: true)
+            resetGame()
+        }
+    }
+    func resetGame() {
+        score = 0
+        questionAsked = 0
+    }
 }
 
